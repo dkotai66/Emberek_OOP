ce42ed1f381bd3b6fd29d2be192fa2113391faec
Kótai Dániel <dkotai66@gmail.com>
2022-09-22 15:46:58 +0200

    fajlok_letrehozva

---
+++ src/Emberek/Ember.java
@@ -0,0 +1,13 @@
+package Emberek;
+
+public class Ember {
+    private String nev;
+    private String szulDatum;
+    private String szulHeLy;
+
+    public Ember(String nev, String szulDatum, String szulHely){
+        this.nev=nev;
+        this.szulDatum=szulDatum;
+        this.szulHeLy=szulHely;
+    }
+}
