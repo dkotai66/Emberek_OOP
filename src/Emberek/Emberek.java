47c77e3627c12013adb9ae906391abd6760f4349
Kótai Dániel <dkotai66@gmail.com>
2022-09-22 16:40:32 +0200

    Emberek osztálynak lista adattag és egy konstruktort hoztunk létre, mely tömb alakján tölti fel a listát

---
+++ src/Emberek/Emberek.java
@@ -0,0 +1,14 @@
+package Emberek;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Emberek {
+    private List<Ember> emberek;
+
+    public Emberek(Ember[] emberTomb) {
+        emberek = new ArrayList<>();
+        this.emberek.addAll(Arrays.asList(emberTomb));
+    }
+}
